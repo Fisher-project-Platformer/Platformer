22ccf2d75c1036c847c7e5c6fc05c5e0913277f4
Joseph Nielson <jlemonade98@gmail.com>
2017-02-17 08:34:36 -0500

    Added methods for draw, getY, and setY

--- Platform.java
+++ Platform.java
@@ -6,37 +6,62 @@
  *
  */
 
+import java.awt.*; //For graphics
 
 public class Platform 
 {
 	//Variables for platform
 	private int platformY = 0; //Vertical location of platform
-	private final int HEIGHT = 20; //Height or thickness of platform
+	final int PLATFORM_HEIGHT = 20; //Height or thickness of platform
 	private int width = 0; //The given width of the applet
 	
 	//Variables for gap
-	private final double GAP_SIZE = 0.10; //Percentage of the width of the platform that the gap occupies
-	private double gapLocation = 0; //The desired location of the gap
+	final int GAP_SIZE = 50; //Width of gap
+	private int gapLocation = 0; //The x-coordinate of the gap
 	
 	/**
 	 * Creates a platform and a gap with the given measurements
-	 * @param width the width of the screen to fill with the platform
-	 * @param verticalLocation the y-coordinate of the desired location for the platform
-	 * @param gapLocation the desired location of the gap. Must be less than width.
+	 * @param width the width of the screen that the platform will fill
+	 * @param yCoordinate the y-coordinate for the top-left corner of the platform
+	 * @param gapLocation the x-coordinate of the left-hand side of the gap. Must be less than width.
 	 */
-	public Platform(int width, int verticalLocation, int gapLocation)
+	public Platform(int width, int yCoordinate, int gapLocation)
 	{
 		this.width = width;
-		this.platformY = verticalLocation;
+		this.platformY = yCoordinate;
 		this.gapLocation = gapLocation;
 	}
 	
 	/**
 	 * Draws the platform
 	 */
-	public draw()
+	public void draw(Graphics g)
 	{
-		//Set color and fill rectangles
+		//Set color
+		g.setColor(Color.blue);
 		
+		//Create 2 rectangles for each platform
+		//Left of gap
+		g.fillRect(0, platformY, gapLocation, PLATFORM_HEIGHT);
+		//Right of gap
+		g.fillRect(gapLocation + GAP_SIZE, platformY, width - gapLocation - GAP_SIZE, PLATFORM_HEIGHT);
+	}
+	
+	/**
+	 * Returns the y-coordinate of the platform.
+	 * @return the y-coordinate of the platform.
+	 */
+	public int getY()
+	{
+		return platformY;
+	}
+	
+	/**
+	 * Set the y-coordinate of the platform
+	 * @param newY the desired y-coordinate of the platform
+	 */
+	public void setY(int newY)
+	{
+		this.platformY = newY;
 	}
 }
