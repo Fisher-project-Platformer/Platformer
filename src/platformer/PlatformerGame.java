cba611d30b2cdb291a85b4bc77cb8385c1f64255
ar9914 <cderfler@mail.swvgs.us>
2017-03-27 20:30:13 -0400

    working on making the character show up

--- src/platformer/PlatformerGame.java
+++ src/platformer/PlatformerGame.java
@@ -4,6 +4,8 @@ import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -30,8 +32,24 @@ public class PlatformerGame extends Applet implements KeyListener
 	//character variables
 	static int charHeight = 20; //width and height of the character
 	static int charWidth = 20;
-	int charY=aHeight-charHeight, charX=0; //the coordinates that describe the character's position
+	int charY=0, charX=0; //the coordinates that describe the character's position
 	Move character1;
+	
+	//timer
+	int minutes = 0; //minutes
+	int seconds = 0; //seconds
+	int textPosx = aWidth - 90; //x position of the text
+	int textPosy = 35; //y position of the text
+	int boxPosx = aWidth - 95; //x position of the box around the timer
+	int boxPosy = 0; //y position of the box around the timer
+	int boxSizex = 95;
+	int boxSizey = 50;
+	int start = (int) System.currentTimeMillis(); //get the CPU time right as the program starts
+	int change; //initialize the CPU time that will refresh itself via the timer
+	Font stringFont = new Font("Monospaced", Font.BOLD, 35); //make the font for the text
+	//pause variables
+	int pauseTimeOne = (int) System.currentTimeMillis();
+	int pauseTimeTwo = 0;
 	
 	//Other variables
 	public Timer timer;
@@ -39,25 +57,29 @@ public class PlatformerGame extends Applet implements KeyListener
   int downwardVelocity = 2;
 	
 	//menu variables
-	boolean isPlaying = false;
+	static boolean isPlaying = false;
 	int menuX = 100;
 	int menuY = 100;
 	int menuWidth = aWidth - 200;
 	int menuHeight = aHeight - 200;
 	Menu openMenu = new Menu(menuX, menuY, menuWidth, menuHeight);
+
+	//boolean isScrolling = false;
 	
-	//timer variables
-	int minutes = 0; //minutes
-	int seconds = 0; //seconds
-	int textPosx = aWidth - 90; //x position of the text
-	int textPosy = 35; //y position of the text
-	int boxPosx = aWidth - 95; //x position of the box around the timer
-	int boxPosy = 0; //y position of the box around the timer
-	int boxSizex = 95; //x dimensions of the box
-	int boxSizey = 50; //y dimensions of the box
-	int start = (int) System.currentTimeMillis(); //get the CPU time right as the program starts
-	int change, change2; //initialize the CPU time that will refresh itself via the timer
-	static Font stringFont = new Font("Monospaced", Font.BOLD, 35); //make the font for the text
+	//background
+	Image background;
+	MediaTracker tr;
+	
+	//character
+	
+	Image characterStillLeft;
+	Image characterStillRight;
+	Image characterRunningLeft;
+	Image characterRunningRight;
+	
+	boolean direction = true; //false is left, true is right
+	boolean stance = false; //false is still, true is running
+
 
 	
 	//code below describes player movement side to side
@@ -74,14 +96,21 @@ public class PlatformerGame extends Applet implements KeyListener
 		public void keyPressed(KeyEvent p)//sets up events for when specific keys are typed
 		{
 			if (p.getKeyCode() == KeyEvent.VK_A)//if up arrow is pressed
+			{
 				Move.a=true;
+				direction = false;
+			}
 			
 			if (p.getKeyCode() == KeyEvent.VK_D)//if down arrow is pressed
+			{
 				Move.d=true;
+				direction = true;
+			}
 			
 			if (p.getKeyCode() == KeyEvent.VK_W)//if the W key is pressed
 				Move.w=true;
 			
+
 			if (p.getKeyChar() == ' ' && isPlaying == true)
 				isPlaying=false;
 			
@@ -89,6 +118,25 @@ public class PlatformerGame extends Applet implements KeyListener
 				isPlaying=true;
 			
 			
+
+			if (p.getKeyChar() == ' ' && isPlaying == true)
+			{
+				isPlaying=false;
+				
+				//timer stuff
+				pauseTimeOne = (int) System.currentTimeMillis();
+				
+			}
+			
+			else if (p.getKeyChar() == ' ' && isPlaying == false)
+			{	
+				isPlaying=true;
+				
+				//timer stuff
+				pauseTimeTwo = (int) System.currentTimeMillis();
+				start += pauseTimeTwo - pauseTimeOne;
+			}
+
 		}//ends keyPressed
 		
 		public void keyTyped(KeyEvent p)
@@ -122,32 +170,69 @@ public class PlatformerGame extends Applet implements KeyListener
 	 */
 	public void paint(Graphics g)
 	{
+		//background
+		tr = new MediaTracker(this);
+		background = getImage(getCodeBase(), 
+				"file:///C:/Users/Ar9914/Documents/GitHub/Platformer/src/"
+				+ "platformer/images/CS%20Background.png");
+		tr.addImage(background, 0);
+		g.drawImage(background, 0, 0, this);
+		
 		//Draw platforms
 		platforms.draw(g);
 		
+		//Draw menu	
+		openMenu.draw(g, this);
+		
+		//Timer
+		g.setColor(Color.gray); //create the box
+		g.fillRect(boxPosx, boxPosy, boxSizex, boxSizey); //fill the box
+		g.setColor(Color.CYAN); //create the text
+		g.setFont(stringFont); //set its font
+		if (seconds >= 10)
+			g.drawString(Integer.toString(minutes) + ":" +
+					Integer.toString(seconds), textPosx, textPosy);
+		//if the seconds are greater than 10, you don't need an extra 0 in the timer
+		
+		if (seconds < 10)
+			g.drawString(Integer.toString(minutes) + ":0" + 
+					Integer.toString(seconds), textPosx, textPosy);
+		//if the seconds are less than 10, you do need that extra 0
 
 		//paints character
-		g.setColor(Color.black);
-		g.fillRect(Move.xPosit,Move.yPosit,charHeight,charWidth);
+		if (direction == true && stance == true) //running facing right
+		{
+			tr.addImage(characterRunningRight, 0);
+			characterRunningRight = getImage(getCodeBase(), 
+	    		  "CS character smol (running) (right).png");
+			g.drawImage(characterRunningRight, Move.xPosit, Move.yPosit, this);
+		}
         
+		else if (direction == false && stance == true) //running facing left
+		{
+			tr.addImage(characterRunningLeft, 0);
+			characterRunningLeft = getImage(getCodeBase(), 
+	    		  "CS character smol (running) (left).png");
+			g.drawImage(characterRunningLeft, Move.xPosit, Move.yPosit, this);
+		}
+		
+		else if (direction == true && stance == false) //still facing left
+		{
+			tr.addImage(characterStillRight, 0);
+			characterStillRight = getImage(getCodeBase(), 
+	    		  "/images/character.png");
+			g.drawImage(characterStillRight, Move.xPosit, Move.yPosit, this);
+		}
+		
+		else if (direction == false && stance == false) //still facing right
+		{
+			tr.addImage(characterStillLeft, 0);
+			characterStillLeft = getImage(getCodeBase(), 
+	    		  "CS character smol facing right(still).png");
+			g.drawImage(characterStillLeft, Move.xPosit, Move.yPosit, this);
+		}
         //s menu
         if(!isPlaying) openMenu.draw(g, this);
-
-      //Timer drawing
-      		g.setColor(Color.black); //create the box
-      		g.fillRect(boxPosx, boxPosy, boxSizex, boxSizey); //fill the box
-      		g.setColor(Color.CYAN); //create the text
-      		g.setFont(stringFont); //set its font
-      		if (seconds >= 10)
-      			g.drawString(Integer.toString(minutes) + ":" +
-      					Integer.toString(seconds), textPosx, textPosy);
-      		//if the seconds are greater than 10, you don't need an extra 0 in the timer
-      		
-      		if (seconds < 10)
-      			g.drawString(Integer.toString(minutes) + ":0" + 
-      					Integer.toString(seconds), textPosx, textPosy);
-      		//if the seconds are less than 10, you do need that extra 0
-
 	}
 	
 	/**
@@ -162,20 +247,31 @@ public class PlatformerGame extends Applet implements KeyListener
 		@Override
 		public void actionPerformed(ActionEvent e) 
 		{
-			//timer
-			change = (int) System.currentTimeMillis();
-			minutes = Timer2.getMinutes(change, start);
-			seconds = Timer2.getSeconds(change, start);
 			Move.tick();//calls the method tick from class move
 
 			if (isPlaying)
 			{
 				platforms.scrollDown(downwardVelocity);
+				Move.scrollDown(downwardVelocity);
 			}
-		
-			repaint();
 			
-
+			//timer
+			
+			change = (int) System.currentTimeMillis();
+			if (isPlaying == true) //functioning normally
+			{
+				minutes = Timer2.getMinutes(change, start);
+				seconds = Timer2.getSeconds(change, start);
+			}
+			
+			if (isPlaying == false) //functioning while paused
+			{
+				minutes = Timer2.getMinutes(pauseTimeOne, start);
+				seconds = Timer2.getSeconds(pauseTimeOne, start);
+			}
+			//update the time
+			
+			repaint();
 		}
 
 	}
